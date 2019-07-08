883649470d6c552903ed0d79fb8d6a84b7e22c8a
jiamei <840987554@qq.com>
2019-07-08 14:21:31 +0800

    only EWMA

--- result for compare code/EWMA.py
+++ result for compare code/EWMA.py
@@ -17,18 +17,18 @@ import tensorflow as tf
 TEST_EXAMPLES=264    #测试集个数： TEST_EXAMPLES + n_steps
 lr = 0.0001          #learning rate，用于梯度下降
 training_iters = 150    #训练的循环次数
-n_steps =252       # time steps
+n_steps =126       # time steps
 u1 = 10                #第一个 LSTM的hidden unit 
 u2 = 4                 #第二个 LSTM的hidden unit 
 u3 = 2                 #第三个 LSTM的hidden unit 
 batch_size = 1         #每一个batch的长度
-pt = 252               #Garch 模型 与 volatility 的 rolling 长度
+pt = 126               #Garch 模型 与 volatility 的 rolling 长度
 youhuaqi = 4         #优化器：1：mse,2:mae,3:hmse,4:hmae
 print(pt)
 #get data
 dateparse = lambda dates:pd.datetime.strptime(dates,'%Y%m%d')  #读取日期格式
 
-data = pd.read_csv("D:/RA/result for teacher/CSI300daily.csv",
+data = pd.read_csv("D:/RA/result for teacher/SSE_daily.csv",
                     sep=',',
                     encoding = "utf-8",
                     parse_dates=['TDATE'],
